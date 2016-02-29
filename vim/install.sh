562196bb764d3f1d931f6744b213bbbe2c5323b5
tmeinders <tmeinders@gmail.com>
2016-02-29 14:08:49 -0500

    Adding new vim submodules for JS

--- vim/install.sh
+++ vim/install.sh
@@ -2,3 +2,11 @@
 # tern needs npm install run in its directory
 cd ~/.dotfiles/vim/vim.symlink/bundle/tern_for_vim
 npm install
+
+
+# set up youcompleteme for JS
+brew install cmake
+cd ~/.dotfiles/vim/vim.symlink/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --tern-completer
+
