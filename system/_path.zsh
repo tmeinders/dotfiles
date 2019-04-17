9fb68a94a181d7b1c7a85eb1bacff19b0aad930b
tmeinders <tmeinders@gmail.com>
2019-04-17 16:59:27 -0400

    Updating to get pyenv into the right place in the path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,7 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
